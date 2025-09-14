77fe5565089bcea2653f0ec6dea18c4f807854cb
Olga Makeikina <omakeykina@gmail.com>
2025-09-13 21:45:18 -0300

    ссфлки меню

--- src/components/header/data.ts
+++ src/components/header/data.ts
@@ -1,9 +1,10 @@
 import { MenuItem } from './types'
 
 export const menuItems: MenuItem[] = [
+  { href: "#promocoes", label: "АКЦИИ" },
   { href: "#master-intro", label: "О мастере" },
-  { href: "#about-gomanic", label: "о GOMANIC" },
-  { href: "#reviews", label: "Отзывы" },
-  { href: "#contacts", label: "Контакты" },
-  { href: "#booking", label: "Записаться" }
+  { href: "#about-gomanic", label: "О платформе" },
+  { href: "#social", label: "Отзывы" },
+  { href: "#como-funciona", label: "Как это работает" },
+  { href: "#agendamento", label: "Записаться" }
 ]
