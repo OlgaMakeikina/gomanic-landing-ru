e0f4b2502ae5664a642ad06079091c0b283c9bf9
Olga Makeikina <omakeykina@gmail.com>
2025-09-24 15:34:10 -0300

    fix: parse masterYandexId to number for window.ym calls

--- src/utils/master-analytics.ts
+++ src/utils/master-analytics.ts
@@ -12,24 +12,24 @@ export async function initMasterAnalytics() {
 
 export const trackMasterFormSubmission = (formType: string): void => {
   if (masterYandexId && typeof window !== 'undefined' && window.ym) {
-    window.ym(masterYandexId, 'reachGoal', 'form_submit', { form_type: formType });
+    window.ym(parseInt(masterYandexId), 'reachGoal', 'form_submit', { form_type: formType });
   }
 };
 
 export const trackMasterButtonClick = (buttonName: string): void => {
   if (masterYandexId && typeof window !== 'undefined' && window.ym) {
-    window.ym(masterYandexId, 'reachGoal', 'button_click', { button_name: buttonName });
+    window.ym(parseInt(masterYandexId), 'reachGoal', 'button_click', { button_name: buttonName });
   }
 };
 
 export const trackMasterBooking = (serviceId: string): void => {
   if (masterYandexId && typeof window !== 'undefined' && window.ym) {
-    window.ym(masterYandexId, 'reachGoal', 'booking_started', { service_id: serviceId });
+    window.ym(parseInt(masterYandexId), 'reachGoal', 'booking_started', { service_id: serviceId });
   }
 };
 
 export const trackMasterContact = (contactType: string): void => {
   if (masterYandexId && typeof window !== 'undefined' && window.ym) {
-    window.ym(masterYandexId, 'reachGoal', 'contact_click', { contact_type: contactType });
+    window.ym(parseInt(masterYandexId), 'reachGoal', 'contact_click', { contact_type: contactType });
   }
 };
