0f09692c2ac4ef9189adf94b8306b9ce0126bc35
Olga Makeikina <omakeykina@gmail.com>
2025-09-26 16:20:42 -0300

    убрали линк о мастере из меню

--- src/components/layout/Header/data.ts
+++ src/components/layout/Header/data.ts
@@ -2,7 +2,6 @@ import { MenuItem } from './types'
 
 export const menuItems: MenuItem[] = [
   { href: "#services", label: "АКЦИИ" },
-  { href: "#master-intro", label: "О мастере" },
   { href: "#about-gomanic", label: "О платформе" },
   { href: "#reviews", label: "Отзывы" },
   { href: "#process", label: "Как это работает" },
